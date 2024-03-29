21c69a50f0bd89378b0304e196c12554004ef46d
nii9006 <neequayefelix123@gmail.com>
2024-03-29 12:02:48 -0700

    reate a dynamic library that contains C functions

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first unt
+ * @b: second int
+ * Return: addiction
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mod of two integers
+ * @a: first unt
+ * @b: second int
+ * Return: addiction
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
